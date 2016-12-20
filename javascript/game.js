0554b65661b24b5183c33c4103e0b2640501fb3b
Christina Ruc <Christina Ruc>
2016-12-20 14:06:40 -0500

    final commit

--- javascript/game.js
+++ javascript/game.js
@@ -1,18 +1,18 @@
-//create the document.on function
+//create the document.ready function
 $(document).ready(function() {
-
-	crystals = ['images\blue-crystal.jpg', 'images\pink-crystal.jpg', 'images\green-crystal.jpg', 'images\yellow-crystal.jpg'];
-
+//get the images
+	crystals = ['images/blue.png', 'images/red.png', 'images/green.png', 'images/yellow.png'];
+//create variables and elements with win and losses id
 	var counter = 0;
 	var wins = 0;
 	var losses = 0;
 	$('#win').text(wins);
 	$('#losses').text(losses);
 
-	newCrystals();
+	crystalImage();
 	newGame();
-
-	function newCrystals () {
+//call the funtion, ask for a random number, 
+	function crystalImage () {
 		var numbers = []
 			while(numbers.length < 4){
 				var randomNumber = Math.ceil(Math.random()*12)
@@ -28,11 +28,11 @@ $(document).ready(function() {
 
 			for (i = 0; i < numbers.length; i++) {
 				var imageCrystal = $('<img>');
-			imageCrystal.attr('data-num', numbers[i]);
-			imageCrystal.attr('src', crystals[i]);
-			imageCrystal.attr('alt', 'crystals');
-			imageCrystal.addClass('crystalImage')
-			$('#crystals').append(imageCrystal);
+				imageCrystal.attr('data-num', numbers[i]);
+				imageCrystal.attr('src', crystals[i]);
+				imageCrystal.attr('alt', 'crystals');
+				imageCrystal.addClass('crystalImage')
+				$('#crystals').append(imageCrystal);
 
 			}
 	}
@@ -40,11 +40,11 @@ $(document).ready(function() {
 	function newGame() {
 		counter = 0;
 		$('#yourScore').text(counter);
-
-		function fandomIntFromInterval(min,max){
+// call the random number for player to guess and reset the numbers for the crystals
+		function randomIntFromInterval(min,max){
 			return Math.floor(Math.random()*(max-min+1)+min);
 			}
-			var numberToGuess = randomIntFromInterval(19,120);
+			var numberToGuess = randomIntFromInterval(19,120);  
 
 			$('.value').text(numberToGuess);
 
@@ -52,22 +52,23 @@ $(document).ready(function() {
 				counter = counter + parseInt($(this).data('num'));
 
 				$('#yourScore').text(counter);
-
+// if the total score is the same is the # to match, read you won and increase wins by 1, restart game
 				if (counter == numberToGuess){
-					$('#status').text('You Won!!');
-					wins ++;
+					$('#status').text('You Won!!'); wins ++;
+					$('#win').text(wins);
 					console.log(wins)
-					$('#crystals').empty();
-					newCrystals();
-					newGame();
 
-				} else if ( counter > numberToGuess){
-					$('#status').text('You Lost!')
-					losses ++;
-					$('#loss').text(losses);
-					console.log(losses)
 					$('#crystals').empty();
-					newCrystals();
+					crystalImage();
+					newGame();
+// if the total score is over the # to match, read you lost and increase losses by 1, restart game
+				} else if ( counter > numberToGuess){
+					$('#status').text('You Lost!');	losses ++;
+					$('#losses').text(losses);
+					console.log(losses)
+
+					$('#crystals').empty();
+					crystalImage();
 					newGame();
 				}
 			});
